9bdc36d5e99c85d80bc8f77c5ff9f51cec08f4d2
goknu <goknu@GKNUR>
2025-12-09 13:58:55 +0300

    migrations

---
+++ DAL/Migrations/20251208142909_newplayer.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class newplayer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TotalShipsSunk",
+                table: "Players",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TotalShotsHit",
+                table: "Players",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalShipsSunk",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "TotalShotsHit",
+                table: "Players");
+        }
+    }
+}
---
+++ DAL/Migrations/20251208143205_oldplayer.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class oldplayer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalShipsSunk",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "TotalShotsHit",
+                table: "Players");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TotalShipsSunk",
+                table: "Players",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TotalShotsHit",
+                table: "Players",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
